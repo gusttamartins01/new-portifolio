25e4a0fff752b70b097766d64d2ff14f5761341f
gusttamartins01 <luizgustams@gmail.com>
2025-08-04 21:29:54 -0300

    fon

--- src/data/projectsData.tsx
+++ src/data/projectsData.tsx
@@ -4,8 +4,8 @@ import { Project } from '../types';
 export const projects: Project[] = [
   {
     id: 1,
-    title: "Jogo interativo de coleta de frutas",
-    description: "Jogo interativo no qual tem coletar o máximo de frutas possíveis sem deixar cair",
+    title: "Jogo interativo para coletar frutas",
+    description: "Jogo interativo no qual tem que coletar o máximo de frutas possíveis sem deixar cair",
     tech: ["HTML5", "CSS3", "JavaScript",],
     image: "/assetes/jogodefrutas.jpg",
     demo: "https://fruit-catcher-game-eta.vercel.app/", // Substitua por um link real
@@ -13,8 +13,8 @@ export const projects: Project[] = [
   },
   {
     id: 2,
-    title: "Sistema web para uma academia e para cadastro de clientes",
-    description: "Sistema que para cadastro de clientes, e feedbacks de melhorias e implementações, e apresentação de uma academia voltado a resultados com visão computacional",
+    title: "Sistema web para uma academia e cadastro de clientes",
+    description: "Sistema para realizar cadastros de clientes, feedbacks de melhorias e implementações, e apresentação de uma academia voltado a resultados com visão computacional",
     tech: ["React", "TypeScript", "Tailwind","Node.js","SupaBase"],
     image: "/assetes/acad.png",
     demo: "https://revo-fit.com/",
@@ -23,7 +23,7 @@ export const projects: Project[] = [
   {
     id: 3,
     title: "Chatbot institucional",
-    description: "Chatboty com IA para uxiliar nas demndas e suporte ao atendimento dos alunos da instutuição",
+    description: "Chatboty com IA para auxiliar nas demndas e suporte ao atendimento dos alunos da instutuição de ensino",
     tech: ["HTML5", "CSS3","JavaScript","API","Node.js", "Inteligência Artificial"],
     image: "/assetes/Chatbot.jpg",
     demo:  "https://chatbot-teste-pearl.vercel.app/",
